cb182d9fd4bfafa8ed9975c9759d698bd97f92b7
Jonatas Laet <jonatas.laet@monitoratec.com.br>
2021-11-15 22:20:54 -0300

    Adicionando uma tabela de dados

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 
 import {InputTextModule} from 'primeng/inputtext';
 import {ButtonModule} from 'primeng/button';
+import { TableModule } from 'primeng/table';
 
 @NgModule({
   declarations: [
@@ -15,7 +16,8 @@ import {ButtonModule} from 'primeng/button';
     BrowserModule,
     AppRoutingModule,
     InputTextModule,
-    ButtonModule
+    ButtonModule,
+    TableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
